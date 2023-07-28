c046d38cfdd08cb63dcc0d08565f6437da5f52f0
Yanka28 <yanachepeluk@ukr.net>
2023-07-28 16:17:18 +0300

    try-catch

---
+++ src/cat-api.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+
+axios.defaults.headers.common["x-api-key"] ='live_K4pzQoLzu9IhHwHlN4Hvo6GlKKLKJ2ktVFgFb1KTRljqdmsjG2dTpJbWjfSQGmNf'
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1'
+
--- src/index.js
+++ src/index.js
@@ -19,64 +19,59 @@ let iD = ''
 let page = 1
 let totalPhoto = 0
 
-// axios.defaults.headers.common["x-api-key"] ='38400956-ed3ce45b124f70d420fda24dd'
-// axios.defaults.baseURL = 'https://pixabay.com/api'
-// key = 38400956-ed3ce45b124f70d420fda24dd
-// ('https://pixabay.com/api/?key=38400956-ed3ce45b124f70d420fda24dd&q=cat&image_type=photo')
-// '/?q=cat&image-type=photo&orientation=horizontal&safesearch=true'
-
-
- function onSubmit(evt) { 
-     evt.preventDefault()
-     gallery.innerHTML = ''
-     iD = evt.currentTarget[0].value
-     fetchPhoto(iD)
-       .then((array) => {
-         gallery.insertAdjacentHTML('beforeend', createMarkup(array))
-         lightbox.refresh()   })
-    
-     btnEl.hidden = false
+async function onSubmit  (evt) {
+  evt.preventDefault() 
+  gallery.innerHTML = ''
+  iD = evt.currentTarget[0].value
+   try {
+    await fetchPhoto(iD)
+      .then((array) => {
+        if (array.length === 0) {
+          Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+        } else {
+          Notiflix.Notify.info(`Hooray! We found ${totalPhoto} images.`) 
+          gallery.insertAdjacentHTML('beforeend', createMarkup(array))
+          lightbox.refresh()
+          btnEl.hidden = false
+        }
+      })
+    } catch (error) {
+          console.log(error.message)
+  }
 }
+  
 
-function onLoad(evt) { 
+async function onLoad(evt) {
+  try {
     page += 1
     if (totalPhoto >= page * 40) {
-        fetchPhoto(iD, page)
-            .then((array) => {
-              gallery.insertAdjacentHTML('beforeend', createMarkup(array))
-              lightbox.refresh()
-              const { height: cardHeight } = document
-                    .querySelector(".gallery")
-                    .firstElementChild.getBoundingClientRect();
-
-               window.scrollBy({
-                    top: cardHeight * 4,
-                    behavior: "smooth",
-                  });
-              })
+      await fetchPhoto(iD, page)
+        .then((array) => {
+          gallery.insertAdjacentHTML('beforeend', createMarkup(array))
+          lightbox.refresh()
+          smoothScrolling()
+        })
+    } else {
+      btnEl.hidden = true
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
     }
-    else { 
-        btnEl.hidden=true
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
-    }
-}
+  } catch (error) {
+        console.log(error.message)}
+  }
 
- async function fetchPhoto(id, num = 1) {
-   const { data } = await axios.get(`https://pixabay.com/api/?key=38400956-ed3ce45b124f70d420fda24dd&q=${id}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${num}`)
-     if (data.hits.length === 0) { Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.") }
-     else { 
-       if (num === 1) { Notiflix.Notify.info( `Hooray! We found ${data.totalHits} images.`)}
-        totalPhoto = data.totalHits
-        const  array = data.hits
-        return array
+  
+async function fetchPhoto(id, num = 1) {
+       const { data } = await axios.get(`https://pixabay.com/api/?key=38400956-ed3ce45b124f70d420fda24dd&q=${id}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${num}`)
+       totalPhoto = data.totalHits
+      const array = data.hits
+         return array
      }
-     
-}
+   
+
 
 function createMarkup(arr) {
     return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-        `
-        <div class="photo-card">
+        `<div class="photo-card">
         <a href="${largeImageURL}">
      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
        </a>
@@ -98,8 +93,16 @@ function createMarkup(arr) {
 }
 
 
+function smoothScrolling() { 
+  const { height: cardHeight } = document
+                    .querySelector(".gallery")
+                    .firstElementChild.getBoundingClientRect();
 
-
-
+               window.scrollBy({
+                    top: cardHeight * 4,
+                    behavior: "smooth",
+                  });
+}
+  
 
  
